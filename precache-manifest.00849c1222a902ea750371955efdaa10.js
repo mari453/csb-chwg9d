8a015a87686e3806c9e214093d72ee74641ac695
Code Sandbox <hello@codesandbox.io>
2022-12-05 02:58:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.00849c1222a902ea750371955efdaa10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f46689ad222cef202e9",
+    "url": "https://mari453.github.io/csb-chwg9d/static/css/main.4baab34e.chunk.css"
+  },
+  {
+    "revision": "6f46689ad222cef202e9",
+    "url": "https://mari453.github.io/csb-chwg9d/static/js/main.6f46689a.chunk.js"
+  },
+  {
+    "revision": "c2b4f0b62ddf6ec6ef3c",
+    "url": "https://mari453.github.io/csb-chwg9d/static/js/1.c2b4f0b6.chunk.js"
+  },
+  {
+    "revision": "39fe7e0a99f8ef49e25a",
+    "url": "https://mari453.github.io/csb-chwg9d/static/js/runtime~main.39fe7e0a.js"
+  },
+  {
+    "revision": "114073c6dadf7e830242998347dede17",
+    "url": "https://mari453.github.io/csb-chwg9d/index.html"
+  }
+];
